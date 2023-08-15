84d08f4d01c069d51102b1b000c73c712e9d5d18
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-15 18:08:15 -0300

    Criando alunos Incompleto

--- src/gerenciador_de_biblioteca/index.ts
+++ src/gerenciador_de_biblioteca/index.ts
@@ -9,6 +9,70 @@ import { Biblioteca } from "./classes/Biblioteca";
 const prompt = require("prompt-sync")();
 
 //  Definição de atributos
+let entrada: Number;
+let aluno: String;
+let professor;
+let biblioteca;
+
+function iniciarPrograma() {
+    console.log("=======================================");
+    console.log("Bem vindo!");
+    console.log("Este é um programa que cadastra alunos e professores para fazer empréstimos, devoluções e buscas...");
+
+    opcoes();
+    function opcoes() {
+        // console.log(aluno);  O aluno é aberto aqui
+        console.log("Escolha uma das seguintes opções:");
+        console.log("1 - Logar como aluno: ");
+        console.log("2 - Logar como professor: ");
+        console.log("3 - Entrar na biblioteca: ");
+        entrada = parseInt(prompt("Sua escolha: "));
+        
+    }
+
+
+    switch (entrada) {
+        case 1:
+            //  Loga como aluno
+            console.log("\nUsuário aluno selecionado!");
+            usuarioAluno();
+            opcoes();
+            break;
+        case 2:
+            //  Loga como professor
+            usuarioProfessor();
+            break;
+        case 3:
+            //  Entra na biblioteca
+            console.log("\nEntrando na biblioteca!");
+            entrarNaBiblioteca(aluno);
+            break;
+
+        default:
+            //  Alerta que o valor inserido está incorreto e reinicia o programa
+            console.log("Você digitou algo errado.\nTente novamente...");
+            iniciarPrograma();
+            break;
+    }
+}
+function usuarioAluno() {
+    // let array = [];  Implementar uma lista que guarda os IDs dos alunos
+    const nome = prompt("Qual o seu nome: ");
+    const id = "#001"
+    aluno = new Usuario(nome, id);
+    return aluno;
+    // console.log(aluno);
+}
+
+function entrarNaBiblioteca(aluno: String) {
+    console.log(`Bem vindo, ${aluno}.`)
+}
+
+
+
+iniciarPrograma();
+
+/*
 let entrada;
 
 function iniciarPrograma() {
@@ -43,7 +107,7 @@ function iniciarPrograma() {
     }
 }
 
-
-
-
-iniciarPrograma();
+function cadastrarAluno() {
+    const novoUsuario = prompt("Qual seu nome de aluno: ")
+}
+*/
