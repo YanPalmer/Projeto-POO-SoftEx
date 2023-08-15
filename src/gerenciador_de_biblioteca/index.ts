0a6b687341b93afcafb6b166186b0402a56b7d26
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-15 15:07:13 -0300

    Criei o código inicial da biblioteca

--- src/gerenciador_de_biblioteca/index.ts
+++ src/gerenciador_de_biblioteca/index.ts
@@ -5,3 +5,45 @@ import { Aluno } from "./classes/Aluno";
 import { Professor } from "./classes/Professor";
 import { Emprestimo } from "./classes/Emprestimo";
 import { Biblioteca } from "./classes/Biblioteca";
+
+const prompt = require("prompt-sync")();
+
+//  Definição de atributos
+let entrada;
+
+function iniciarPrograma() {
+    console.log("Bem vindo!");
+    console.log("Este é um programa que cadastra alunos e professores para fazer empréstimos, devoluções e buscas...");
+
+    console.log("Escolha uma das seguintes opções:");
+    console.log("1 - Cadastrar aluno: ");
+    console.log("2 - Cadastrar professor: ");
+    console.log("3 - Entrar na biblioteca: ");
+    entrada = prompt("Sua escolha: ");
+
+    switch (entrada) {
+        case 1:
+            //  Cadastra o aluno
+            cadastrarAluno();
+            break;
+        case 2:
+            //  Cadastra o professor
+            cadastrarProfessor();
+            break;
+        case 3:
+            //  Entra na biblioteca
+            entrarNaBiblioteca();
+            break;
+    
+        default:
+            //  Alerta que o valor inserido está incorreto e reinicia o programa
+            console.log("Você digitou algo errado.\nTente novamente...");
+            iniciarPrograma();
+            break;
+    }
+}
+
+
+
+
+iniciarPrograma();
